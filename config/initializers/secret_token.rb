af446a93ca87601a6df49348a1de4257a2774134
Diego Santamaria <dsantamaria@icloud.com>
2013-04-23 14:49:17 -0300

    initial submit
    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Precios::Application.config.secret_token = '4a8c751b7030360cf8a0163539da9c8fcc9ee989e55e1a2c02471717c804c5f241cef8cdda77806f6714633c1733447fa97e139e2045b8516984cc2839a47def'
