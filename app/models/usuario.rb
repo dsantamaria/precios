a79b50e0eac91f904f41ca26bdf681cd37b7c8a4
Diego Santamaría <dsantamaria@gmail.com>
2013-04-24 00:01:49 -0300

    environment sync, usuarios CRUD
    read nots about rails/ruby versions on both environments

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,7 @@
 class Usuario < ActiveRecord::Base
   attr_accessible :lastlogin, :login, :pass
+  
+  validates :login,  :presence => true
+  validates :pass, :presence => true,
+                      :length => { :minimum => 5 }
 end
