c685a97e3ca82b12841c196eae938585b699b29f
Diego Santamaría <dsantamaria@gmail.com>
2013-04-26 00:01:07 -0300

    added password functionality

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,7 +1,8 @@
 class Usuario < ActiveRecord::Base
-  attr_accessible :lastlogin, :login, :pass
+  attr_accessible :lastlogin, :login, :pass, :pass_confirmation
   
   validates :login,  :presence => true
   validates :pass, :presence => true,
                       :length => { :minimum => 5 }
+  validates_confirmation_of :pass
 end
